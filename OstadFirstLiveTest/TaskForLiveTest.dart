5b08bb294a78f5630f89680bd1d4dacb4125b8f8
Md Farhad Hossen Imon <farhadimon8@gmail.com>
2024-06-24 23:06:42 +0600

    Updated

--- OstadFirstLiveTest/TaskForLiveTest.dart
+++ OstadFirstLiveTest/TaskForLiveTest.dart
@@ -1,3 +1,26 @@
-void main(){
-  print('Imon');
+class Media {
+  void play() {
+    print("Playing media..");
+  }
+}
+
+
+class Song extends Media {
+
+  String artist;
+
+  Song(this.artist);
+
+  @override
+  void play() {
+    print("Playing song by $artist..");
+  }
+}
+
+void main() {
+  Media media = Media();
+  media.play();
+
+  Song song = Song("Atif Aslam");
+  song.play();
 }
